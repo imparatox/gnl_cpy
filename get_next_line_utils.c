f3b4e92673a912598bf5e1c2373c04c5de3463a3
mimparat <mimparat@f1br5s6.paris.42.school>
2026-05-30 07:29:28 +0200

    Commit home

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -41,6 +41,46 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (s3);
 }
 
+char	*ft_strchr(char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (str)
+	{
+		while (str[i])
+		{
+			if (str[i] == (char)c)
+				return ((char *)&str[i]);
+			i++;
+		}
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+	}
+	return (NULL);
+}
+
+char	*ft_before(char *str)
+{
+	size_t	i;
+	char	*before;
+
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	before = malloc(i + 2);
+	if (!before)
+		return (NULL);
+	i = 0;
+	while(str[i] != '\n')
+	{
+		before[i] = str[i];
+		i++;
+	}
+	before[i] = '\0';
+	return (before);
+}
+
 #include <stdio.h>
 
 int	main(void)
